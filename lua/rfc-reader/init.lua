d29ed19034e4232f0977a36eb5628a12cd812ef3
Michael Paulson <mpaulson@netflix.com>
2020-12-27 18:04:07 -0800

    fixes all the things

--- lua/rfc-reader/init.lua
+++ lua/rfc-reader/init.lua
@@ -14,9 +14,10 @@ function parseResponse(body)
 end
 
 function createBuffer(lines)
-    local bufh = vim.fn.nvim_create_buf(true, false)
+    local bufh = vim.fn.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_option(bufh, "filetype", "rfc")
 
+    vim.api.nvim_buf_set_option(bufh, "modifiable", true)
     vim.api.nvim_buf_set_lines(bufh, 0, #lines, false, lines)
     vim.api.nvim_buf_set_option(bufh, "modifiable", false)
 
@@ -26,12 +27,17 @@ end
 M.get = function()
 
     local rfc = vim.fn.input("Rfc Number > ");
+
+    if not rfc or rfc == "" then
+        return
+    end
+
     local cache_money = rfc_reader_cache[rfc]
-    print("Cache", rfc, cache_money)
 
     -- TODO: If they close the buffer, you are effd.  Gotta clear the cache
     if cache_money then
-        vim.api.nvim_win_set_buf(0, cache_money.bufh)
+        local bufh = createBuffer(cache_money.contents)
+        vim.api.nvim_win_set_buf(0, bufh)
     else
         local out = curl.request({
             url = "cht.sh/rfc/" .. rfc
@@ -42,7 +48,6 @@ M.get = function()
 
         rfc_reader_cache[rfc] = {
             contents = lines,
-            bufh = bufh,
         }
 
         vim.api.nvim_win_set_buf(0, bufh)
